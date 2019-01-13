9075a2e09e60592021c477260b19a417a6930f51
albertomisail <misailalberto@gmail.com>
2019-01-12 20:33:57 -0800

    Added implementation for block.h

--- pa1/block.cpp
+++ pa1/block.cpp
@@ -1,2 +1,41 @@
 #include "block.h"
 
+namespace cs221util{
+
+Block::build(PNG & im, int column, int width){
+  width_ = width;
+  height_ = im.height();
+  for (unsigned x = column; x < column + width; x++) {
+    vector < HSLAPixel > col;
+    for (unsigned y = 0; y < im.height(); y++) {
+      col.push_back(im.getPixel(x,y));
+    }
+    data.push_back(col);
+  }
+}
+
+Block::render(PNG & im, int column){
+  for(unsigned x = 0; x < width_; x++){
+    for(unsigned y = 0; y < height_; y++){
+      im.getPixel(x+column, y) = data.at(x).at(y);
+    }
+  }
+}
+
+Block::greyscale(){
+  for (unsigned x = 0; x < width_; x++){
+    for (unsigned y = 0; y < height_; y++){
+      data.at(x).at(y)->s=0;
+    }
+  }
+}
+
+Block::width(){
+  return width_;
+}
+
+Block::height(){
+  return height_;
+}
+
+}
