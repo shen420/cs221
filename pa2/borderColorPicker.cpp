56215b2b0a50e1844b5cdb40d71175f7707f6a9a
albertomisail <misailalberto@gmail.com>
2019-02-14 14:20:30 -0800

    pa2

---
+++ pa2/borderColorPicker.cpp
@@ -0,0 +1,13 @@
+#include "borderColorPicker.h"
+
+borderColorPicker::borderColorPicker(HSLAPixel fillColor, PNG & img, double tolerance,HSLAPixel center) : color(fillColor), im(img), tol(tolerance), ctr(center)
+{
+/* Your code here! */
+}
+
+HSLAPixel borderColorPicker::operator()(int x, int y)
+{
+
+/* Your code here! */
+  
+}
